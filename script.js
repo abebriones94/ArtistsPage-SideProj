b081769f53d4baa4fe680bb1a2aa06467ff58d65
Abraham Briones <abebriones94@gmail.com>
2022-08-20 15:16:08 +0200

    Added JavaScript to change the way the button clicks allow the page to run up and down

---
+++ script.js
@@ -0,0 +1,35 @@
+// Bring in everything we need
+
+const sliderContainer = document.querySelector(".slider-container"); // The entire container
+const slideRight = document.querySelector(".right-slide");
+const slideLeft = document.querySelector(".left-slide");
+const upButton = document.querySelector(".up-button");
+const downButton = document.querySelector(".down-button");
+const slidesLength = slideRight.querySelectorAll("div").length; // Bringing in all the divs that are in the slide 'right' (background images)
+
+let activeSlideIndex = 0;
+
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`; // Negative because I want it to go up. I want to get the LAST index. Remember index is 0 based. Multipled by 100 because it takes up 100%
+
+upButton.addEventListener("click", () => changeSlide("up"));
+downButton.addEventListener("click", () => changeSlide("down"));
+
+const changeSlide = (direction) => {
+  const sliderHeight = sliderContainer.clientHeight; // Will give slider height
+  if (direction === "up") {
+    activeSlideIndex++;
+    if (activeSlideIndex > slidesLength - 1) {
+      activeSlideIndex = 0;
+    }
+}   else if (direction === "down") {
+      activeSlideIndex--;
+      if (activeSlideIndex < 0) {
+        activeSlideIndex = slidesLength -1;
+    
+    }
+  }
+
+  slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`;
+  slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`;
+
+}
