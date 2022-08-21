617777a7c33dcf2929d1e271959ffafa04f94cd9
Abraham Briones <abebriones94@gmail.com>
2022-08-21 08:20:32 +0200

    Added comments to script.js

--- script.js
+++ script.js
@@ -9,29 +9,29 @@ const slidesLength = slideRight.querySelectorAll("div").length; // Bringing in a
 
 let activeSlideIndex = 0;
 
-slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`; // Negative because I want it to go up. I want to get the LAST index. Remember index is 0 based. Multipled by 100 because it takes up 100%
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`; // Negative because I want it to go up. I want to get the LAST index. Remember index is 0 based. Multipled by 100 because it takes up 100%. Basically this puts the slides in the correct locations I need them in 
 
 upButton.addEventListener("click", () => changeSlide("up"));
 downButton.addEventListener("click", () => changeSlide("down"));
 
-const changeSlide = (direction) => {
-  const sliderHeight = sliderContainer.clientHeight; // Will give slider height
+const changeSlide = (direction) => { // Taking in direction as parameter  
+  const sliderHeight = sliderContainer.clientHeight; // Will give slider height based on screen size (dynamic) 
   if (direction === "up") {
-    activeSlideIndex++;
-    if (activeSlideIndex > slidesLength - 1) {
+    activeSlideIndex++; // Increment by 1 
+    if (activeSlideIndex > slidesLength - 1) { // If it gets to the end, it goes back to the beginning 
       activeSlideIndex = 0;
     }
-  } else if (direction === "down") {
-    activeSlideIndex--;
-    if (activeSlideIndex < 0) {
-      activeSlideIndex = slidesLength - 1;
+  } else if (direction === "down") { 
+    activeSlideIndex--; // Take away 1 
+    if (activeSlideIndex < 0) { // If less than 0, it gets sent to the last index 
+      activeSlideIndex = slidesLength - 1; 
     }
   }
 
-  slideRight.style.transform = `translateY(-${
+  slideRight.style.transform = `translateY(-${  // Allows it to move up and down. It will translate on the Y Axis. (Negative so it can go up) 
     activeSlideIndex * sliderHeight
   }px)`;
-  slideLeft.style.transform = `translateY(${
+  slideLeft.style.transform = `translateY(${ // Positive allows it go to the opposite up as above 
     activeSlideIndex * sliderHeight
   }px)`;
 };
